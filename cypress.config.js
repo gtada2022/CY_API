75824de64d0f251b0115bec6fa6d03ab1c4248cd
gtada2022 <gtada8529@icloud.com>
2022-09-05 10:31:27 -0300

    Api test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'x1rhcu',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    baseUrl: 'https://simple-books-api.glitch.me'
+  }
+})
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,35 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('API test automated', () => {
+
+  it('Add a new pet to the store', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://fakerestapi.azurewebsites.net/api/v1/Users',
+    }).then(res => {
+      console.log(res)
+    })
   })
+
+  it('verify the request returns the correct status code', () => {
+    cy.request('https://fakerestapi.azurewebsites.net/api/v1/Users').its('status').should('be.equal', 200)
+  })
+
+  it('verify the request returns 10 items', () => {
+    cy.request('https://fakerestapi.azurewebsites.net/api/v1/Users').its('body').should('have.length', 10)
+  })
+
+  it('Verify the new item exists exists', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://fakerestapi.azurewebsites.net/api/v1/Users',
+    }).then(res => {
+      expect(res.body).is.not.empty
+      expect(res.status).to.equal(200)
+      expect(res.body[0]).to.have.property('id', 1)
+      expect(res.body[0]).to.have.property('userName', 'User 1')
+      expect(res.body[0]).to.have.property('password', 'Password1')
+    })
+  })
+
+
+
 })
